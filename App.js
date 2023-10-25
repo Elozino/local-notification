29f2a4a3e920c5f4bf4dd1f870a20f787109f964
Elozino <ovedhee@gmail.com>
2023-10-25 12:52:32 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, useRef } from "react";
+import { Text, View, Button, Platform, StyleSheet } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 export default function App() {
+  async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: "Here is the notification body",
+        data: { data: "goes here" },
+      },
+      trigger: { seconds: 120 },
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={styles.container}
+    >
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>Your device is {Device.osName}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          console.log('first')
+          await schedulePushNotification();
+        }}
+      />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
